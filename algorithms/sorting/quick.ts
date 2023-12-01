40c8cc36fca874b7acf612a868e1f77fdacc5444
Jakhongiirr <ravshanovjahongir26@gmail.com>
2023-12-01 20:36:14 +0500

    quick sort

---
+++ algorithms/sorting/quick.ts
@@ -0,0 +1,41 @@
+function pivot(
+  arr: number[],
+  start: number = 0,
+  end: number = arr.length - 1
+): number {
+  const swap = (arr: number[], idx1: number, idx2: number) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(
+  arr: number[],
+  left: number = 0,
+  right: number = arr.length - 1
+): number[] {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]));
