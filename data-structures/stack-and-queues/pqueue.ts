873d17954910e49cc6aa3e5ba7f6629161169f04
Jakhongiirr <ravshanovjahongir26@gmail.com>
2024-01-11 14:33:48 +0500

    priority queue

---
+++ data-structures/stack-and-queues/pqueue.ts
@@ -0,0 +1,97 @@
+// Priority queue
+// Pre requistes: Binary heap (/data-structures/heaps/bheap.ts)
+// Imagine hospital and patients, there are people who have just hurting arm or something,
+// and there are some people doctors need to perform a surgery asap.
+
+class Node {
+  val: any;
+  priority: number;
+  constructor(val: any, priority: number) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  private values: any[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val: any, priority: number) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  private bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  private sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+const pq = new PriorityQueue();
+
+pq.enqueue("common cold", 5);
+pq.enqueue("gunshot wound", 1);
+pq.enqueue("high fever", 4);
+pq.enqueue("broken arm", 2);
+pq.enqueue("glass in foot", 3);
+
+console.log("Priority queue:", pq)
+
+for(let i = 0; i < 5; i++) {
+    console.log("\nDequeued:", pq.dequeue())
+}
